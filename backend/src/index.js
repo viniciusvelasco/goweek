ee357de6df1652158e7dc0e2acf7e73676ce21b7
Vinicius Alves Velasco <vinicius.velasco@outlook.com>
2018-11-06 22:18:45 -0300

    Conexão com o mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect("mongodb://goweek:goweek123@ds041177.mlab.com:41177/goweek-viniciusvelasco", 
+{
+  useNewUrlParser: true
+});
 
 app.get('/', (req, res) => {
   return res.send('Hello World');
